72f90ccecc2289b120a9a9f4c9a6809cc1b919c8
Cbaird26 <cbaird26@gmail.com>
2024-05-29 18:03:36 -0400

    Update quantum_ai.py

--- quantum_ai.py
+++ quantum_ai.py
@@ -12,3 +12,21 @@ def run_quantum_circuit(params):
         return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))
 
     return circuit(params)
+from qiskit import IBMQ, Aer, execute
+from qiskit import QuantumCircuit
+
+def run_qiskit_circuit():
+    IBMQ.load_account()
+    provider = IBMQ.get_provider(hub='ibm-q')
+    backend = provider.get_backend('ibmq_qasm_simulator')
+
+    circuit = QuantumCircuit(2, 2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.measure([0, 1], [0, 1])
+
+    job = execute(circuit, backend)
+    result = job.result()
+    counts = result.get_counts()
+
+    return counts
